1b9634ee00affbe581699d6cb98a5a39f8f68cd3
jgramoll <jgramoll@instructure.com>
2018-12-10 20:18:18 -0700

    fix main spacing

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-        "github.com/hashicorp/terraform/plugin"
-        "github.com/hashicorp/terraform/terraform"
-        "github.com/jgramoll/terraform-provider-spinnaker/spinnaker"
+  "github.com/hashicorp/terraform/plugin"
+  "github.com/hashicorp/terraform/terraform"
+  "github.com/jgramoll/terraform-provider-spinnaker/spinnaker"
 )
 
 func main() {
-        plugin.Serve(&plugin.ServeOpts{
-                ProviderFunc: func() terraform.ResourceProvider {
-                        return spinnaker.Provider()
-                },
-        })
+  plugin.Serve(&plugin.ServeOpts{
+    ProviderFunc: func() terraform.ResourceProvider {
+      return spinnaker.Provider()
+    },
+  })
 }
