3643c36bf9accf2824a089efcceed11f2f7a3b4e
Alan Voiski <alannunesv@gmail.com>
2019-06-14 17:02:42 -0700

    Revert "Define capacity fields to string expressions"
    This reverts commit 8c3b4b9058b417a0b19cb2bee86d323a32969bb8.

--- provider/capacity.go
+++ provider/capacity.go
@@ -1,10 +1,7 @@
 package provider
 
 type capacity struct {
-	Desired           int    `mapstructure:"desired"`
-	DesiredExpression string `mapstructure:"desired_expression"`
-	Max               int    `mapstructure:"max"`
-	MaxExpression     string `mapstructure:"max_expression"`
-	Min               int    `mapstructure:"min"`
-	MinExpression     string `mapstructure:"min_expression"`
+	Desired string `mapstructure:"desired"`
+	Max     string `mapstructure:"max"`
+	Min     string `mapstructure:"min"`
 }
--- provider/deploy_stage_cluster_capacity.go
+++ provider/deploy_stage_cluster_capacity.go
@@ -1,8 +1,6 @@
 package provider
 
 import (
-	"strconv"
-
 	"github.com/jgramoll/terraform-provider-spinnaker/client"
 )
 
@@ -10,44 +8,14 @@ func fromClientCapacity(clientCapacity *client.Capacity) *[]*capacity {
 	if clientCapacity == nil {
 		return nil
 	}
-	newCapacity := capacity{}
-	if n, err := strconv.Atoi(clientCapacity.Max); err == nil {
-		newCapacity.Max = n
-	} else {
-		newCapacity.MaxExpression = clientCapacity.Max
-	}
-	if n, err := strconv.Atoi(clientCapacity.Min); err == nil {
-		newCapacity.Min = n
-	} else {
-		newCapacity.MinExpression = clientCapacity.Min
-	}
-	if n, err := strconv.Atoi(clientCapacity.Desired); err == nil {
-		newCapacity.Desired = n
-	} else {
-		newCapacity.DesiredExpression = clientCapacity.Desired
-	}
+	newCapacity := capacity(*clientCapacity)
 	return &[]*capacity{&newCapacity}
 }
 
 func toClientCapacity(c *[]*capacity) *client.Capacity {
 	if c != nil {
 		for _, c := range *c {
-			newCapacity := client.Capacity{}
-			if c.MaxExpression != "" {
-				newCapacity.Max = c.MaxExpression
-			} else {
-				newCapacity.Max = strconv.Itoa(c.Max)
-			}
-			if c.MinExpression != "" {
-				newCapacity.Min = c.MinExpression
-			} else {
-				newCapacity.Min = strconv.Itoa(c.Min)
-			}
-			if c.DesiredExpression != "" {
-				newCapacity.Desired = c.DesiredExpression
-			} else {
-				newCapacity.Desired = strconv.Itoa(c.Desired)
-			}
+			newCapacity := client.Capacity(*c)
 			return &newCapacity
 		}
 	}
