4b5f0beaecc7e615101df8e446a73072f2b73910
jgramoll <jgramoll@gmail.com>
2019-11-21 11:39:32 -0600

    Andrein traffic management (#43)
    * Add traffic management options
    
    * Fix traffic management options
    
    When deploying services/ingresses using the deploy manifest stage,
    the deployment fails if the namespace is empty.
    
    * fix serialize test for traffic management

--- provider/traffic_management_options.go
+++ provider/traffic_management_options.go
@@ -3,7 +3,10 @@ package provider
 import "github.com/jgramoll/terraform-provider-spinnaker/client"
 
 type trafficManagementOptions struct {
-	EnableTraffic bool `mapstructure:"enable_traffic"`
+	EnableTraffic bool     `mapstructure:"enable_traffic"`
+	Namespace     string   `mapstructure:"namespace"`
+	Services      []string `mapstructure:"services"`
+	Strategy      string   `mapstructure:"strategy"`
 }
 
 func newTrafficManagementOptions() *trafficManagementOptions {
@@ -17,6 +20,9 @@ func toClientTrafficManagementOptions(options *[]*trafficManagementOptions) *cli
 		for _, o := range *options {
 			newOptions := client.NewTrafficManagementOptions()
 			newOptions.EnableTraffic = o.EnableTraffic
+			newOptions.Namespace = o.Namespace
+			newOptions.Services = o.Services
+			newOptions.Strategy = o.Strategy
 			return newOptions
 		}
 	}
@@ -28,6 +34,9 @@ func fromClientTrafficManagementOptions(options *client.TrafficManagementOptions
 	if options != nil {
 		o := newTrafficManagementOptions()
 		o.EnableTraffic = options.EnableTraffic
+		o.Namespace = options.Namespace
+		o.Services = options.Services
+		o.Strategy = options.Strategy
 		newArray = append(newArray, o)
 	}
 	return &newArray
